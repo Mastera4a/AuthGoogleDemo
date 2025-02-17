9cdf94309cbf8e205dc4fa7ce9094562601dfaf5
Mikhail Miakishev <kotarolinn@gmail.com>
2025-02-17 05:58:48 +0200

    Add project files.

---
+++ AuthGoogleDemo/GoogleAuth/GoogleAuthService.cs
@@ -0,0 +1,7 @@
+﻿namespace AuthGoogleDemo.GoogleAuth
+{
+    public partial class GoogleAuthService : IGoogleAuthService
+    {
+        private const string WebApiKey = "869044993501-j61vr5g6e43tbu9acftckl2saq63q7ui.apps.googleusercontent.com";
+    }
+}

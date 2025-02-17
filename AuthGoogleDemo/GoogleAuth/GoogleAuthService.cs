e35134c029342b01d52082a1094e4d9ceaf6540c
Mikhail Miakishev <kotarolinn@gmail.com>
2025-02-17 22:34:53 +0200

    Removed my API key

--- AuthGoogleDemo/GoogleAuth/GoogleAuthService.cs
+++ AuthGoogleDemo/GoogleAuth/GoogleAuthService.cs
@@ -2,6 +2,6 @@
 {
     public partial class GoogleAuthService : IGoogleAuthService
     {
-        private const string WebApiKey = "685632098541-g8ndojf5me4fjge2kmqqm6hovc89qf6b.apps.googleusercontent.com";
+        private const string WebApiKey = "{YOUR_API_KEY}";
     }
 }

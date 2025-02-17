10b71f77eea40104e231b460e8a78cb11d91ec4d
Mikhail Miakishev <kotarolinn@gmail.com>
2025-02-17 22:11:54 +0200

    Fixed mistake with not working google sign in on real device

--- AuthGoogleDemo/GoogleAuth/GoogleAuthService.cs
+++ AuthGoogleDemo/GoogleAuth/GoogleAuthService.cs
@@ -2,6 +2,6 @@
 {
     public partial class GoogleAuthService : IGoogleAuthService
     {
-        private const string WebApiKey = "869044993501-j61vr5g6e43tbu9acftckl2saq63q7ui.apps.googleusercontent.com";
+        private const string WebApiKey = "685632098541-g8ndojf5me4fjge2kmqqm6hovc89qf6b.apps.googleusercontent.com";
     }
 }
--- AuthGoogleDemo/MainPage.xaml.cs
+++ AuthGoogleDemo/MainPage.xaml.cs
@@ -6,6 +6,8 @@ namespace AuthGoogleDemo
     {
         private readonly IGoogleAuthService _googleAuthService = new GoogleAuthService();
 
+        int count = 0;
+
         public MainPage()
         {
             InitializeComponent();
@@ -13,15 +15,19 @@ namespace AuthGoogleDemo
 
         private async void LoginBtn_Clicked(object sender, EventArgs e)
         {
-            var loggedUser = await _googleAuthService.GetCurrentUserAsync();  
+            var loggedUser = await _googleAuthService.GetCurrentUserAsync();
 
             if (loggedUser == null)
             {
                 loggedUser = await _googleAuthService.AuthenticateAsync();
             }
 
-            await Application.Current.MainPage.DisplayAlert("Login Message", "Welcome " + loggedUser.FullName, "Ok");
+            // Проверяем null и приводим к строке
+            string userName = loggedUser.FullName?.ToString() ?? "User";
+
+            await Application.Current.MainPage.DisplayAlert("Login Message", "Welcome " + userName, "Ok");
         }
+
 
         private async void logoutBtn_Clicked(object sender, EventArgs e)
         {
@@ -29,5 +35,15 @@ namespace AuthGoogleDemo
 
             await Application.Current.MainPage.DisplayAlert("Login Message", "Goodbay", "Ok");
         }
+
+        //private async void OnCounterClicked(object sender, EventArgs e)
+        //{
+        //    count++;
+
+        //    if (count == 1) CounterBtn.Text = $"Clicked {count} time";
+        //    else CounterBtn.Text = $"Clicked {count} times";
+
+        //    SemanticScreenReader.Announce(CounterBtn.Text);
+        //}
     }
 }
